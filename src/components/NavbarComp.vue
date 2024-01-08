fa2b8d96354eaf2d68526a927253e4bc6b04505a
sphathimandla-makhanya <makhanyasm11@gmail.com>
2024-01-08 13:55:24 +0200

    added text

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -13,23 +13,23 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="home">Home</a>
+          <router-link to="/home" class="nav-link" href="home">Home</router-link>
            
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="about">About</a>
+          <router-link to="/about" class="nav-link" href="about">About</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="resume">Resume</a>
+          <router-link to="/resume" class="nav-link" href="resume">Resume</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="projects">Projects</a>
+          <router-link to="/projects" class="nav-link" href="projects">Projects</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="review">Reviews</a>
+          <router-link to="/review" class="nav-link" href="review">Reviews</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="contact">Contacts</a>
+          <router-link to="/contact" class="nav-link" href="contact">Contacts</router-link>
         </li>
       </ul>
     </div>
