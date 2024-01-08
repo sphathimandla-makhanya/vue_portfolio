9d5c60c8a50aa192b4fa8a7b09ca5ce9e8836a1c
sphathimandla-makhanya <makhanyasm11@gmail.com>
2024-01-08 16:34:10 +0200

    Added json folder and file

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,23 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
 
 export default createStore({
   state: {
+    about:[]
   },
   getters: {
   },
   mutations: {
+    setAbout(state,data){
+      state.about=data
+    }
   },
   actions: {
+    fetchData(context){
+      axios.get('http://localhost:3000/about')
+      .then(a => console.log(a.data);
+      context.commit("setAbout", a.data))
+    }
   },
   modules: {
   }
