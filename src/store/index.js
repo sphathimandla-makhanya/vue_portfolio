f950ca0d477f992bab28888aef1e0808f20d5316
sphathimandla-makhanya <makhanyasm11@gmail.com>
2024-01-09 16:18:28 +0200

    displayed data in views

--- src/store/index.js
+++ src/store/index.js
@@ -3,20 +3,47 @@ import axios from 'axios';
 
 export default createStore({
   state: {
-    about:[]
+    about:[],
+    resume:[],
+    reviews:[],
+    projects:[]
   },
   getters: {
   },
   mutations: {
     setAbout(state,data){
       state.about=data
+    },
+    setResume(state,data){
+      state.resume=data
+    },
+    setProject(state,data){
+      state.projects=data
+    },
+    setReviews(state,data){
+      state.reviews=data
     }
   },
   actions: {
     fetchData(context){
       axios.get('http://localhost:3000/about')
-      .then(a => console.log(a.data);
-      context.commit("setAbout", a.data))
+      .then((a) => {console.log(a.data[0]),
+      context.commit("setAbout", a.data[0])})
+    },
+    resumeData(context){
+      axios.get('http://localhost:3000/resume')
+      .then((cv) => {console.log(cv.data[1]),
+      context.commit("setResume", cv.data[1])})
+    },
+    projectsData(context){
+      axios.get('http://localhost:3000/projects')
+      .then((proj) => {console.log(proj.data),
+      context.commit("setProject", proj.data)})
+    },
+    reviewsData(context){
+      axios.get('http://localhost:3000/reviews')
+      .then((testimonial) => {console.log(testimonial.data),
+      context.commit("setReviews", testimonial.data)})
     }
   },
   modules: {
--- src/views/ResumeViews.vue
+++ src/views/ResumeViews.vue
@@ -1,10 +1,28 @@
 <template>
   <h1>Resume</h1>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <p>{{ $store.state.resume.description1 }}</p> 
+        <img :src="$store.state.resume.img1" alt="">
+      </div>
+    <div class="col">
+      <p>{{ $store.state.resume.description1 }}</p> 
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-
+computed: {
+  resumeData(){
+    this.$store.dispatch('resumeData')
+  }
+},
+mounted(){
+  this.resumeData
+}
 }
 </script>
 
