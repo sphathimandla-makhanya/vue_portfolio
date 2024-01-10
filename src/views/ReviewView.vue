fa7bbc15cc6789b1056cbade99f52f11d5cf6292
sphathimandla-makhanya <makhanyasm11@gmail.com>
2024-01-10 12:04:37 +0200

    added review cards

--- src/views/ReviewView.vue
+++ src/views/ReviewView.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Reviews</h1>
   <div class="container">
-    <!-- <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
       <div v-for="review in $store.state.reviews" :key="review" class="col">
         <div class="card">
           <div class="card bg-black">
@@ -13,21 +13,9 @@
           </div>
         </div>
       </div>
-  </div> -->
-</div>
-<div class="container">
-<div class="row">
-  <div v-for="review in $store.state.reviews" :key="review" class="col">
-  <div class="col">{{ review.review }}</div>
-  <!-- <div class="col-sm-4">col-sm-4</div> -->
   </div>
 </div>
-<div class="row">
-  <div class="col-sm">col-sm</div>
-  <div class="col-sm">col-sm</div>
-  <div class="col-sm">col-sm</div>
-</div>
-</div>
+
 </template>
 
 <script>
